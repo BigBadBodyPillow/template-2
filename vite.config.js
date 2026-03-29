ff3f47ee20044b46d8ab2099f4755f2016173b1c
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-03-29 13:07:42 +0200

    update vite config for hosting

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/template-2/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
